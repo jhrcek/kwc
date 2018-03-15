805841db48486c11b064100011f141c651fa12f9
Jan Hrček <jhrcek@redhat.com>
2018-03-15 05:13:34 +0100

    Implement GET /spaces/{spaceName}

--- app/Main.hs
+++ app/Main.hs
@@ -5,9 +5,10 @@
 module Main where
 
 import Data.Proxy (Proxy (Proxy))
+import Data.Text (Text)
 import Model.Space
 import Network.HTTP.Client (defaultManagerSettings, newManager)
-import Servant.API ((:>), BasicAuth, BasicAuthData (BasicAuthData), Get, JSON)
+import Servant.API ((:<|>) ((:<|>)), (:>), BasicAuth, BasicAuthData (BasicAuthData), Capture, Get, JSON)
 import Servant.Client (BaseUrl (BaseUrl), ClientM, Scheme (Http), client, mkClientEnv, runClientM)
 
 main :: IO ()
@@ -16,11 +17,18 @@ main = do
     let auth = BasicAuthData "testadmin" "admin1234;"
         env = mkClientEnv mgr (BaseUrl Http "localhost" 8080 "business-central/rest")
     runClientM (getSpaces auth) env >>= print
+    runClientM (getSpace auth "myteam") env >>= print
 
-type API = BasicAuth "KIE Workbench Realm" () :> "spaces" :> Get '[JSON] [Space]
+
+type API =
+       BasicAuth "KIE Workbench Realm" () :> "spaces" :> Get '[JSON] [Space]
+  :<|> BasicAuth "KIE Workbench Realm" () :> "spaces" :> Capture "spaceName" Text :> Get '[JSON] Space
+
 
 api :: Proxy API
 api = Proxy
 
+
 getSpaces :: BasicAuthData -> ClientM [Space]
-getSpaces = client api
+getSpace  :: BasicAuthData -> Text -> ClientM Space
+getSpaces :<|> getSpace = client api
